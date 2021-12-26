de577c5155d617983dd0bcf49d8775862b392b16
Renato Silva <dev.renatosilva@gmail.com>
2021-12-26 12:05:19 -0300

    solving test

--- src/ProductStructure.php
+++ src/ProductStructure.php
@@ -20,7 +20,27 @@ class ProductStructure
 
     public function make(): array
     {
-       //todo your code.
-        return [];
+        //todo your code.
+        $products = ProductStructure::products;
+        $result = [];
+
+        for($i = 0, $length = count($products, 0) ; $i < $length; $i++){
+        
+            $color_size = explode("-", $products[$i]);
+            $color = $color_size[0];
+            $size = $color_size[1];
+    
+            if(!array_key_exists($color, $result)){
+                $result[$color] = [
+                ];
+            } 
+    
+            if(!array_key_exists($size, $result[$color])){
+                $result[$color][$size] = 1;
+            } else {
+                $result[$color][$size] += 1;
+            }
+        }
+        return $result;
     }
-}
+} 
